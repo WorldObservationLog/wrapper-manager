03d2f10808c551c0a56a1d3e54a7e76fbe4b17a4
WorldObservationLog <wolchannel@proton.me>
2025-07-14 17:17:55 +0800

    fix: Improper conversion of numbers to strings causes two-step verification to fail to work properly also fixed #1

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ import (
 	"os"
 	"os/user"
 	"slices"
+	"strconv"
 	pb "wrapper-manager/proto"
 )
 
@@ -81,7 +82,7 @@ func (s *server) Login(stream grpc.BidiStreamingServer[pb.LoginRequest, pb.Login
 			}
 		}
 		if req.Data.TwoStepCode != 0 {
-			provide2FACode(id, string(req.Data.TwoStepCode))
+			provide2FACode(id, strconv.Itoa(int(req.Data.TwoStepCode)))
 		} else {
 			LoginConnMap.Store(id, stream)
 			go WrapperInitial(req.Data.Username, req.Data.Password)
