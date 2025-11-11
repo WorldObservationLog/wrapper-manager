0fd55aa146dc78bd11ab0286178875880c21da78
WorldObservationLog <wolchannel@proton.me>
2025-11-11 12:52:09 +0800

    fix: m3u8 deadlock

--- m3u8.go
+++ m3u8.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"net"
 	"sync"
+	"time"
 )
 
 var Lock = sync.Map{}
@@ -16,27 +17,43 @@ func GetM3U8(instance *WrapperInstance, adamId string) (string, error) {
 	lock, _ := Lock.LoadOrStore(instance.Id, &sync.Mutex{})
 	lock.(*sync.Mutex).Lock()
 	defer lock.(*sync.Mutex).Unlock()
-	conn, err := net.Dial("tcp", fmt.Sprintf("127.0.0.1:%d", instance.M3U8Port))
+
+	conn, err := net.DialTimeout("tcp", fmt.Sprintf("127.0.0.1:%d", instance.M3U8Port), 5*time.Second)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("dial timeout or error: %w", err)
 	}
 	defer conn.Close()
+
+	deadline := time.Now().Add(10 * time.Second)
+
+	if err := conn.SetWriteDeadline(deadline); err != nil {
+		return "", fmt.Errorf("set write deadline error: %w", err)
+	}
+
 	_, err = conn.Write([]byte{byte(len(adamId))})
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("conn write 1 error: %w", err)
 	}
+
 	_, err = io.WriteString(conn, adamId)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("conn write 2 (WriteString) error: %w", err)
 	}
+
+	if err := conn.SetReadDeadline(deadline); err != nil {
+		return "", fmt.Errorf("set read deadline error: %w", err)
+	}
+
 	response, err := bufio.NewReader(conn).ReadBytes('\n')
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("conn read error: %w", err)
 	}
+
+	_ = conn.SetReadDeadline(time.Time{})
+
 	if len(response) > 0 {
 		response = bytes.TrimSpace(response)
 		return string(response), nil
-
 	} else {
 		return "", errors.New("empty response")
 	}
